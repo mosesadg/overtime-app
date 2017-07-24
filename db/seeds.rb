03a370c35ae0bcdae9ba5302267c6de0c2652365
Anand <moses.adg@gmail.com>
2017-07-23 23:29:34 -0400

    updated admin dashboard for status

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,11 @@
 @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name:"John", last_name:"Snow")
 
 puts "1 User created"
+
+AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name:"Admin", last_name:"Name")
+
+puts "1 Admin User created"
+
 100.times do |post|
 	Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end

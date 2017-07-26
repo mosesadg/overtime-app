91d99867c7686da22797ed5b5de8e73f2d7d20ab
Anand <moses.adg@gmail.com>
2017-07-26 17:15:57 -0400

    phone validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,26 +9,39 @@ RSpec.describe User, type: :model do
   describe "creation" do
   	it "can be created" do
   		expect(@user).to be_valid
-	end	
-
-	it "cannot be created without first_name" do
-		@user.first_name = nil
-		expect(@user).to_not be_valid
-	end
-
-    it "cannot be created without last_name" do
-      @user.last_name = nil
-      expect(@user).to_not be_valid
-    end
-    it "cannot be created without phone" do
-      @user.phone = nil
-      expect(@user).to_not be_valid
-    end
+	   end	
   end
 
-  describe 'custom name methods' do
-    it 'has a full name method that combines first and last name' do
-      expect(@user.full_name).to eq("SNOW, JON")
+  describe "validations" do
+    it "cannot be created without first_name" do
+      @user.first_name = nil
+      expect(@user).to_not be_valid
     end
-  end
+
+      it "cannot be created without last_name" do
+        @user.last_name = nil
+        expect(@user).to_not be_valid
+      end
+      it "cannot be created without phone" do
+        @user.phone = nil
+        expect(@user).to_not be_valid
+      end
+
+      it 'requires the phone attr to only contain integers' do
+        @user.phone = "mygreatstr"
+        expect(@user).to_not be_valid
+      end
+
+      it 'requires the phone attr to only have 10 charaters' do
+        @user.phone = "12345678901"
+        expect(@user).to_not be_valid
+      end
+
+    end
+
+    describe 'custom name methods' do
+      it 'has a full name method that combines first and last name' do
+        expect(@user.full_name).to eq("SNOW, JON")
+      end
+    end
 end

9ace64cf1997c7af4836995e08eff33a6f54c2a4
Anand <moses.adg@gmail.com>
2017-07-31 23:57:21 -0400

    live data for admin homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,11 @@ AdminUser.create(email: "admin@test.com",
 puts "1 Admin User created"
 
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+	Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, 
+		consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+			nulla pariatur.", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
@@ -35,3 +39,5 @@ puts "100 Posts have been created"
 end
 
 puts "100 audit logs have been created"
+
+
